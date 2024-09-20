b5cc68276eb9fa5f1d1455c6eef2eb4d29e180ce
anuj upadhyay <anuju760@gmail.com>
2024-09-20 19:10:05 +0530

    resolved some of the bugs in the deployment  of CodeCanvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import EditorComponent from '@/components/EditorComponent'
-import { ModeToggleBtn } from '@/components/mode-toggle-btn'
+
 import React from 'react'
 
 export default function Home() {
--- components/SelectLanguages.tsx
+++ components/SelectLanguages.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { languageOptions } from "@/config/config";
